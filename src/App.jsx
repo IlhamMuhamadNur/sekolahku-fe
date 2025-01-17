4ef4cf1d7faab8e0d82ff2ffbb8ad41a08d512d3
nylanisya <naylaanisya7@gmail.com>
2025-01-17 17:52:18 +0700

    perubahan app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "./components/login-form";
+import LoginForm from "./components/loginform";
 import Dashboard from "./components/dashboard";
+import OTPForm from "./components/resetpasswordotp";
+import OpeningPage from "./components/openingpage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" element={<OpeningPage />} />
+        <Route path="/loginform" element={<LoginForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/resetpasswordotp" element={<OTPForm />} />
       </Routes>
     </Router>
   );
