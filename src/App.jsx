3de28fc3cf52db2fb4781de562bcb528b6db2206
seil456 <viseilaazrivania17929@gmail.com>
2025-01-21 17:11:19 +0700

    penyesuaian dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "./components/login-form";
+import LoginForm from "./components/loginform";
 import AdminDashboard from "./components/AdminDashboard";
+import OTPForm from "./components/resetpasswordotp";
+import OpeningPage from "./components/openingpage";
+import NewPassword from "./components/NewPassword";
+import Register from "./components/Register";
+import EBookPage from "./components/ebook";
 import ERaport from "./components/ERaport";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" element={<OpeningPage />} />
+        <Route path="/loginform" element={<LoginForm />} />
         <Route path="/admindashboard" element={<AdminDashboard />} />
-        <Route path="/" element={<ERaport />} />
+        <Route path="/resetpasswordotp" element={<OTPForm />} />
+        <Route path="/NewPassword" element={<NewPassword />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/ebook" element={<EBookPage />} />
+        <Route path="/ERaport" element={<ERaport />} />
       </Routes>
     </Router>
   );
