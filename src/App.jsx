99d2368c02dc9073915aeb86f9bb233984aafeff
seil456 <viseilaazrivania17929@gmail.com>
2025-01-24 17:07:57 +0700

    halaman data admin

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoginForm from "./views/pages/loginform";
 import OTPForm from "./views/pages/resetpasswordotp";
 import OpeningPage from "./views/pages/openingpage";
 import AdminDashboard from "./views/pages/AdminDashboard";
+import DataAdmin from "./views/pages/DataAdmin";
 import NewPassword from "./views/pages/NewPassword";
 import Register from "./views/pages/Register";
 import EBookPage from "./views/pages/ebook";
@@ -13,11 +14,12 @@ function App() {
   return (
     <Router>
       <Routes>
-      //
+        //
         <Route path="/" element={<OpeningPage />} />
         <Route path="/login-form" element={<LoginForm />} />
         <Route path="/reset-passwordotp" element={<OTPForm />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/data-admin" element={<DataAdmin />} />
         <Route path="/new-password" element={<NewPassword />} />
         <Route path="/register" element={<Register />} />
         <Route path="/e-book" element={<EBookPage />} />
