085b701aee7e5634d461414e1e7d55b93a2ce566
nylanisya <naylaanisya7@gmail.com>
2025-01-28 17:44:51 +0700

    penamaan file dan perubahan pada app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "./views/pages/login-form";
 import OTPForm from "./views/pages/reset-passwordotp";
-import OpeningPage from "./views/pages/openingpage";
+import OpeningPage from "./views/pages/opening-page";
 import AdminDashboard from "./views/pages/admin-dashboard";
-import DataAdmin from "./views/pages/DataAdmin";
-import NewPassword from "./views/pages/NewPassword";
-import Register from "./views/pages/Register";
-import EBookPage from "./views/pages/ebook";
-import ERaport from "./views/pages/ERaport";
+import DataAdmin from "./views/pages/data-admin";
+import NewPassword from "./views/pages/new-password";
+import Register from "./views/pages/register";
+import EBookPage from "./views/pages/e-book";
+import ERaport from "./views/pages/e-raport";
 
 function App() {
   return (
@@ -23,7 +23,7 @@ function App() {
         <Route path="/new-password" element={<NewPassword />} />
         <Route path="/register" element={<Register />} />
         <Route path="/e-book" element={<EBookPage />} />
-        <Route path="/e-Raport" element={<ERaport />} />
+        <Route path="/e-raport" element={<ERaport />} />
       </Routes>
     </Router>
   );
